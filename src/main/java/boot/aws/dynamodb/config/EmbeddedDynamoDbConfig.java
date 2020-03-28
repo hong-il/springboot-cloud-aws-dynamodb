fc664d42b52907165b6b5cfa68d0bde6348ee4aa
KIM HONGIL <clroyan01@gmail.com>
2020-03-29 02:38:58 +0900

    Create config - embedded dynamodb

---
+++ src/main/java/boot/aws/dynamodb/config/EmbeddedDynamoDbConfig.java
@@ -0,0 +1,52 @@
+package boot.aws.dynamodb.config;
+
+import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@Profile("local") // (1)
+@ConditionalOnProperty(name = "embedded-dynamodb.use", havingValue = "true") // (2)
+public class EmbeddedDynamoDbConfig {
+
+    private DynamoDBProxyServer server;
+
+    @PostConstruct
+    public void start() {
+        if (server != null) {
+            return;
+        }
+
+        try {
+            //AwsDynamoDbLocalTestUtils.initSqLite(); // (3)
+            server = ServerRunner.createServerFromCommandLineArgs(new String[]{"-inMemory"});
+            server.start();
+            log.info("Start Embedded DynamoDB");
+        } catch (Exception e) {
+            throw new IllegalStateException("Fail Start Embedded DynamoDB", e);
+        }
+    }
+
+    @PreDestroy
+    public void stop() {
+        if (server == null) {
+            return;
+        }
+
+        try {
+            log.info("Stop Embedded DynamoDB");
+            server.stop();
+        } catch (Exception e) {
+            throw new IllegalStateException("Fail Stop Embedded DynamoDB", e);
+        }
+    }
+}
