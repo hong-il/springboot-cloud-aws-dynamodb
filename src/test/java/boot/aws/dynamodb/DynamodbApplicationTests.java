444da22287186a1fb60c47ba60f339ac43d331b0
KIM HONGIL <clroyan01@gmail.com>
2020-03-30 02:28:58 +0900

    Create aws dynamodb project

---
+++ src/test/java/boot/aws/dynamodb/DynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.aws.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
