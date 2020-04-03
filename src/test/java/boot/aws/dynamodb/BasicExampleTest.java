78171c068b39129a611decf3ef55dbef9261f19e
KIM HONGIL <clroyan01@gmail.com>
2020-04-03 11:50:50 +0900

    Create table test - create table

---
+++ src/test/java/boot/aws/dynamodb/BasicExampleTest.java
@@ -0,0 +1,45 @@
+package boot.aws.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestPropertySource(properties = "embedded-dynamodb.use=true")
+public class BasicExampleTest {
+
+    @Autowired
+    private AmazonDynamoDB dynamoDB;
+
+    private CreateTableResult createTable(String tableName, String hashKeyName) {
+        List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
+        attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.S));
+
+        List<KeySchemaElement> ks = new ArrayList<>();
+        ks.add(new KeySchemaElement(hashKeyName, KeyType.HASH));
+
+        ProvisionedThroughput provisionedthroughput = new ProvisionedThroughput(1000L, 1000L);
+
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName(tableName)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withKeySchema(ks)
+                .withProvisionedThroughput(provisionedthroughput);
+
+        return dynamoDB.createTable(request);
+    }
+}
